ff4f1ad5b54e9ace65a559506029324eb6fa589a
Cal Stephens <calstephens98@gmail.com>
2015-01-11 17:24:00 -0500

    UI concept

---
+++ Squareify/VideoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VideoViewController.swift
+//  Squareify
+//
+//  Created by Cal on 1/11/15.
+//  Copyright (c) 2015 Cal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVKit
+
+class VideoViewController : AVPlayerViewController {
+    
+}
--- Squareify/ViewController.swift
+++ Squareify/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movingSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches {
+            updateSelectionPosition((touch as UITouch).locationInView(self.view).y)
+            break
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches {
+            updateSelectionPosition((touch as UITouch).locationInView(self.view).y)
+            break
+        }
+    }
+    
+    func updateSelectionPosition(y : CGFloat) {
+        let frameHeight = movingSquare.frame.size.height
+        let squareHeight = frameHeight / 3
+        let screenHeight = self.view.frame.height
+        var newY = y - frameHeight / 2
+        
+        //keep center square from going off top of screen
+        if newY < -squareHeight {
+            newY = -squareHeight
+        }
+        
+        //keep center square from going off bottom of screen
+        if newY > -(2 * squareHeight - screenHeight) {
+            newY = -(2 * squareHeight - screenHeight)
+        }
+        
+        movingSquare.frame.origin.y = newY
+    }
+    
 }
 
