fcffcb911b6742847ecb209fd92344d948b29f7c
Cal Stephens <calstephens98@gmail.com>
2015-01-11 15:46:37 -0500

    Initial Commit

---
+++ Squareify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Squareify
+//
+//  Created by Cal on 1/11/15.
+//  Copyright (c) 2015 Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
